cae5a5dc007b8ba665a95a5cfedcff3f9af59fe3
JetWen <killer500k@gmail.com>
2024-04-07 11:53:18 +0800

    Add Some header files to use printk

--- 1_chrdevbase/chrdevbase.c
+++ 1_chrdevbase/chrdevbase.c
@@ -1,20 +1,23 @@
 #include <linux/module.h>
-
+#include <linux/kernel.h>
+#include <linux/init.h>
 
 static int __init chardevbase_init(void)
 {
+    printk("Chrdevbase init\r\n");
     return 0;
 }
 static void __exit chardevbase_exit(void)
 {
-
+    printk("Chrdevbase exit\r\n");
 }
-
-
-
 
 /* 
  * Module Entry & Module Exit
  */
 module_init(chardevbase_init);  /* Module Entry */
 module_exit(chardevbase_exit);  /* Module Exit  */
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("JetWen");
