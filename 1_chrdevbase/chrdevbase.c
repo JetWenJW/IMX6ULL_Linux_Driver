7b7403c4e4f797c361112601a3110f5db5e8e9d6
JetWen <killer500k@gmail.com>
2024-04-07 16:14:48 +0800

    Accomplish chardevbase Driver & APP Test OK

--- 1_chrdevbase/chrdevbase.c
+++ 1_chrdevbase/chrdevbase.c
@@ -2,9 +2,18 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/fs.h>
+#include <linux/slab.h>
+#include <linux/io.h>
+#include <linux/uaccess.h>
 
 #define CHARDEVBASE_MAJOR    200            /* Major Chaedevbase number */
 #define CHARDEVBASE_NAME     "chrdevbse"    /* Name of chaedevbase      */
+
+static char readbuffer[100];                /* Read Buffer              */
+static char writebuffer[100];               /* Write Buffer             */
+static char kerneldata = {"kelnel Data"};
+
+
 
 static int chardevbase_open(struct inode *inode, struct file *filp)
 {
@@ -20,13 +29,35 @@ static int chardevbase_release(struct inode *inode, struct file *filp)
 
 static ssize_t chardevbase_read(struct file *filp, __user char *buf, size_t count, loff_t *ppos)
 {
-    printk("chardevbase read\r\n");
+    int ret = 0;
+    //printk("chardevbase read\r\n");
+    memcpy(readbuffer, kerneldata, sizrof(kerneldata));
+    copy_to_user(buf, readbuffer, count);
+
+    if(ret == 0)
+    {
+        printk("Kernel Send Data OK!!\r\n");
+    }
+    else
+    {
+        printk("Kernel Send Data Fail!!\r\n");
+    }
     return 0;
 }
 
 static ssize_t chardevbase_write(struct file *filp, __user char *buf, size_t count, loff_t *ppos)
 {
-    printk("chardevbase Write\r\n");
+    int ret = 0;
+    //printk("chardevbase Write\r\n");
+    copy_from_user(writebuffer, buf, count);
+    if(ret == 0)
+    {
+        printk("Kernel Receive Data : %s\r\n", writebuffer);
+    }
+    else
+    {
+        printk("Kernel Receive Data Fail!!\r\n");
+    }
     return 0;
 }
 
--- 1_chrdevbase/chrdevbaseAPP.c
+++ 1_chrdevbase/chrdevbaseAPP.c
@@ -3,11 +3,15 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*
  * argc : the number of Arguments
  * argv[] : the arguments of actions,typically char data type
- * ./chardevbaseAPP <filesname>
+ * ./chardevbaseAPP <filesname> <1 : Read> <2 : Write>
+ * ./chardevbaseAPP/dev/chardevbase 1 Read
+ * ./chardevbaseAPP/dev/chardevbase 2 Write
  */
 int main(int argc, char *argv[])
 {
@@ -15,7 +19,14 @@ int main(int argc, char *argv[])
     int fd = 0;
     char *filename;
     char readbuffer[100], writebuffer[100];
+    static char userdata[] = {"User Data!"};
 
+
+    if(argc != 3)
+    {
+        printf("Error Usage!\r\n");
+        return -1;
+    }
 
     filename = argv[1];
 
@@ -26,13 +37,37 @@ int main(int argc, char *argv[])
         printf("Can't Open files %s\r\n", filename);
         return -1;
     }
-    /* Read Files */
-    ret = read(fd, readbuffer, 50);
-    if(ret < 0)
+    
+    if(atoi(argv[2]) == 1)/* Read */
     {
-        printf("Read Fail %s\r\n", filename);
+        /* Read Files */
+        ret = read(fd, readbuffer, 50);
+        if(ret < 0)
+        {
+            printf("Read Fail %s\r\n", filename);
+        }
+        else
+        {
+            printf("APP Read Data :%s\r\n", readbuffer);
+        }
     }
 
+    
+    if(atoi(argv[2]) == 2)/* Write */
+    {
+        memcpy(writebuffer, userdata, sizeof(userdata));
+        /* Write Files */
+        ret = write(fd, writebuffer, 50);
+        if(ret < 0)
+        {
+            printf("Write Fail %s\r\n", filename);
+        }
+        else
+        {
+            printf("Write successed~");
+        }
+
+    }
 
     /* Write Files */
     ret = write(fd, writebuffer, 50);
